fd540044aa29f391da29063a886c1d423fae2bb7
Charlie <jnr.31gcharlieedokpa@gmail.com>
2023-02-22 12:17:39 +0100

    hice una refactorizacion para introducir un método

--- src/apuestas/Apuestas.java
+++ src/apuestas/Apuestas.java
@@ -11,6 +11,12 @@ public class Apuestas {
         Apuesta laApuesta;
         int mi_dinero;
 
+        operativa_Apuesta();
+    }
+
+    public static void operativa_Apuesta() {
+        Apuesta laApuesta;
+        int mi_dinero;
         laApuesta = new Apuesta(1000, 4, 2);
         try {
             System.out.println("Apostando...");
@@ -18,7 +24,6 @@ public class Apuestas {
         } catch (Exception e) {
             System.out.println("Fallo al realizar la Apuesta");
         }
-
         try {
             System.out.println("Intento cobrar apuesta segun el resultado del partido");
             laApuesta.cobrar_apuesta(2, 3);
